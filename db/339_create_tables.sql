3a03e03d716a2af81812a590f3c1a83d03cd4b9e
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-02 12:46:25 +0000

    Generated microservice version null

---
+++ db/339_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 339, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table test.
+--
+CREATE TABLE commedit.test (
+  test INTEGER ,
+CONSTRAINT test_PK PRIMARY KEY (test)
+ 
+);
+
+
+
